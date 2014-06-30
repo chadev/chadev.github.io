4ffcaa398d33f8cb2c6766de60952488a8f0fb6d
kylegordy <kyle@kylegordydesign.com>
2014-06-29 23:12:41 -0400

    hacked on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,61 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var notify = require('gulp-notify');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+var cp = require('child_process');
 
-// run jekyll build – http://stackoverflow.com/a/21403905
-gulp.task('jekyll-build', function () {
-  require('child_process').spawn('jekyll', ['build'], {stdio: 'inherit'});
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+
+// build the jekyll site
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
 });
 
-// compile sass and check for errors
+// rebuild jekyll and do page reload
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+// compile sass into both _site/assets/css (for live injecting) and site (for future jekyll builds)
 gulp.task('compile-sass', function () {
     return gulp.src('assets/sass/**/*.scss')
         .pipe(sass({ sourcemap: true, style: 'compressed'}))
         .on("error", notify.onError(function (error) {
             return "Dang! " + error.message;
         }))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css'))
+        .pipe(gulp.dest('_site/assets/css'));
+});
+
+// minify PNG, JPEG, GIF and SVG images
+gulp.task('imagemin', function () {
+    return gulp.src('assets/images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('_site/assets/images/'));
 });
 
 // start a server and watch for html and css changes
-gulp.task('browser-sync', function() {  
-    browserSync.init(["_site/assets/css/**/*.css", "_site/**/*.html"], {
+gulp.task('browser-sync', ['compile-sass', 'jekyll-build'], function() {  
+    browserSync.init(['_site/assets/css/*.css'], {
         server: {
-            baseDir: "./_site/"
+            baseDir: '_site'
         }
     });
 });
 
 // watch for changes
-gulp.task('default', ['jekyll-build', 'compile-sass', 'browser-sync'], function () {  
-    gulp.watch(["assets/sass/**/*.scss"], ['compile-sass']);
-    gulp.watch([ "*.yml", "*.md", "_devs/*.md", "*.html", "_includes/*.html", "_layouts/*.html", "assets/**/*"], ['jekyll-build']);
+gulp.task('default', ['imagemin', 'browser-sync'], function () {  
+    gulp.watch(['assets/sass/**/*.scss'], ['compile-sass']);
+    gulp.watch(['assets/images/**/*'], ['imagemin']);
+    gulp.watch([ '*.yml', '*.md', '_devs/*.md', '*.html', '_includes/*.html', '_layouts/*.html'], ['jekyll-rebuild']);
 });

6c4dd4f29de3a63d24dc80fd9b0cbf785ac9c2a6
William Roush <william.roush@roushtech.net>
2014-12-30 22:51:21 -0500

    Fixes a build error.

--- _scripts/ci/run_deploy.sh
+++ _scripts/ci/run_deploy.sh
@@ -2,17 +2,15 @@
 BRANCH=${GH_BRANCH-gh-pages}
 USERNAME=${GH_USER-Travis-CI}
 EMAIL=${GH_EMAIL-travis@travis-ci.org}
-node build.js
-(
-  mkdir travis-output-repo
-  git clone "https://${GH_TOKEN}@${GH_REF}" travis-output-repo
-  cd travis-output-repo
-  git config user.name "${USERNAME}"
-  git config user.email "${EMAIL}"
-  git checkout --orphan ${BRANCH}
-  git rm -rf .
-  cp -R ../_site/* ./
-  git add -A
-  git commit -m "Deployed to Github Pages"
-  git push --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
-)
+
+mkdir travis-output-repo
+git clone "https://${GH_TOKEN}@${GH_REF}" travis-output-repo
+cd travis-output-repo
+git config user.name "${USERNAME}"
+git config user.email "${EMAIL}"
+git checkout --orphan ${BRANCH}
+git rm -rf .
+cp -R ../_site/* ./
+git add -A
+git commit -m "Deployed to Github Pages"
+git push --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
